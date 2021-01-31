b8b434ea01206f79a60524a97fdef2f3c20676fc
ofir-danan <77383793+ofir-danan@users.noreply.github.com>
2021-02-01 00:08:28 +0200

    some changes to pass the tests

--- src/main1.js
+++ src/main1.js
@@ -1,39 +1,85 @@
 "use-strict";
-// const { Request, default: fetch } = require("node-fetch");
 let counter = Number(document.getElementById("counter").innerHTML);
 let itemsArray = []; // this array will contain the objects for the localStorage
 let BIN_ID = "6016e96d0ba5ca5799d1b648";
 let url = `https://api.jsonbin.io/v3/b/${BIN_ID}`;
 let geturl = `https://api.jsonbin.io/v3/b/${BIN_ID}/latest`;
-// let API_KEY = `$2b$10$cRzyEQSOZdt/fuOMckt6ROz.jxlLbODODX6ElNIoUyP95gTbEWOIK`;
 
 async function putData() {
   const sendObject = {
     "my-todo": itemsArray,
-    counter: counter,
+    // counter: counter,
   };
-
-  let init = {
+  const response = await fetch(url, {
     method: "PUT",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(sendObject),
-  };
-  const request = new Request(url, init);
-  const response = await fetch(request);
+  });
   return response.json();
 }
 
 async function getData() {
-  let init = {
+  const response = await fetch(geturl, {
     method: "GET",
-  };
-  const request = new Request(geturl, init);
-  const response = await fetch(request);
+  });
   const body = await response.json();
-  return body.record;
+  return body.record["my-todo"];
 }
+
+// this will check if the localStorage contain information and if it is it will print it
+window.addEventListener("DOMContentLoaded", function () {
+  getData().then((result) => {
+    console.log(result);
+    let dataFromJSONBin = result;
+    if (dataFromJSONBin.length === 0) return;
+    // let dataFromJSONBin["my-todo"] = dataFromJSONBin["my-todo"];
+    counter = dataFromJSONBin.length;
+    itemsArray = dataFromJSONBin;
+    const counterSpan = document.getElementById("counter");
+    counterSpan.innerText = dataFromJSONBin.length;
+    const listSection = document.querySelector("#view-section");
+    for (let i = 0; i < dataFromJSONBin.length; i++) {
+      console.log(dataFromJSONBin[i]["todo-text"]);
+      //add the container div
+      const todoContainer = document.createElement("div");
+      todoContainer.classList.add("todo-container");
+      listSection.appendChild(todoContainer);
+
+      // adding data-percentage and class to sort them later by priority
+      todoContainer.setAttribute(
+        "data-percentage",
+        dataFromJSONBin[i]["todo-priority"]
+      );
+
+      // adding a check box
+      const taskCheck = document.createElement("input");
+      taskCheck.setAttribute("type", "checkbox");
+      taskCheck.className = "taskCheck";
+      todoContainer.appendChild(taskCheck);
+
+      // adding the text
+      const todoText = document.createElement("div");
+      todoText.classList.add("todo-text");
+      todoContainer.appendChild(todoText);
+
+      // adding the time
+      const todoCreatedAt = document.createElement("div");
+      todoCreatedAt.classList.add("todo-created-at");
+      todoContainer.appendChild(todoCreatedAt);
+
+      // adding the priority
+      const todoPriority = document.createElement("div");
+      todoPriority.classList.add("todo-priority");
+      todoContainer.appendChild(todoPriority);
+
+      todoText.innerText = dataFromJSONBin[i]["todo-text"];
+      todoCreatedAt.innerText = dataFromJSONBin[i]["todo-created-at"];
+      todoPriority.innerText = dataFromJSONBin[i]["todo-priority"];
+    }
+  });
+});
 
 const listSection = document.querySelector("#view-section");
 
@@ -133,61 +179,62 @@ sortButton.addEventListener("click", function () {
     .appendTo(warpingDiv);
 });
 
-// DELETE BUTTON
-const deleteButton = document.getElementById("delete-button");
-deleteButton.addEventListener("click", function () {
-  let flag = false; //will change once the confirmation will be approved
-  let confirmation = false;
-  $(".taskCheck").each(async function () {
-    if ($(this).is(":checked")) {
-      if (!flag) {
-        confirmation = confirm("Are you sure you want to delete this items?");
-        flag = true;
-      }
-      if (confirmation === true) {
-        //if confirmed continue
-        let parent = $(".taskCheck:checked").closest(".todo-container");
+// // DELETE BUTTON
+// const deleteButton = document.getElementById("delete-button");
+// deleteButton.addEventListener("click", function () {
+//   let flag = false; //will change once the confirmation will be approved
+//   let confirmation = false;
+//   $(".taskCheck").each(async function () {
+//     if ($(this).is(":checked")) {
+//       if (!flag) {
+//         confirmation = confirm("Are you sure you want to delete this items?");
+//         flag = true;
+//       }
+//       if (confirmation === true) {
+//         //if confirmed continue
+//         let parent = $(".taskCheck:checked").closest(".todo-container");
 
-        //taking all the innerText of the div that was selected in order
-        //to find the wanted item in the storage
-        for (let i = 0; i < parent.length; i++) {
-          let newItemsFromJSONBin = await getData();
-          // getting the values from the localStorage
-          let divInnerText = parent[i].innerText;
-          //modify the date to look the same
-          let arrayDiv = divInnerText.split("\n");
-          let declarationDate = arrayDiv[1].split(" ").join("");
-          let localStorageIndex = newItemsFromJSONBin;
-          let newItems = []; //new array that contains all the un-deleted items
-          for (let i = 0; i < localStorageIndex.length; i++) {
-            let localValues = Object.values(localStorageIndex[i]);
-            //modify the date to look the same
-            let declarationDateStorage = localValues[1].split(" ").join("");
+//         //taking all the innerText of the div that was selected in order
+//         //to find the wanted item in the storage
+//         for (let i = 0; i < parent.length; i++) {
+//           await getData().then((result) => {
+//             let newItemsFromJSONBin = result;
+//             // getting the values from the localStorage
+//             console.log(parent[i].innerText);
+//             let divInnerText = parent[i].innerText;
+//             //modify the date to look the same
+//             let arrayDiv = divInnerText.split("\n");
+//             let declarationDate = arrayDiv[1].split(" ").join("");
+//             let localStorageIndex = newItemsFromJSONBin;
+//             let newItems = []; //new array that contains all the un-deleted items
+//             for (let i = 0; i < localStorageIndex.length; i++) {
+//               let localValues = Object.values(localStorageIndex[i]);
+//               //modify the date to look the same
+//               let declarationDateStorage = localValues[1].split(" ").join("");
 
-            //comparing the strings of the date in order to find the parallel item
-            if (declarationDate.localeCompare(declarationDateStorage) !== 0) {
-              newItems.push(localStorageIndex[i]);
-            }
-          }
+//               //comparing the strings of the date in order to find the parallel item
+//               if (declarationDate.localeCompare(declarationDateStorage) !== 0) {
+//                 newItems.push(localStorageIndex[i]);
+//               }
+//             }
 
-          //putting the items that use not deleted in the storage
-
-          //change the counter
-          document.getElementById("counter").innerText = newItems.length;
-          itemsArray = newItems;
-          counter = newItems.length;
-          await putData(itemsArray, counter);
-        }
-        //removing from the page without reload
-        $(".taskCheck:checked").closest(".todo-container").remove();
-      }
-    }
-    return;
-  });
-  if (!confirmation) {
-    alert("please select items to delete");
-  }
-});
+//             //change the counter
+//             document.getElementById("counter").innerText = newItems.length;
+//             itemsArray = newItems;
+//             counter = itemsArray.length;
+//           });
+//         }
+//         //removing from the page without reload
+//         $(".taskCheck:checked").closest(".todo-container").remove();
+//         await putData(itemsArray);
+//       }
+//     }
+//     return;
+//   });
+//   if (!confirmation) {
+//     alert("please select items to delete");
+//   }
+// });
 
 //ADD BUTTON FUNCTION
 const addButton = document.querySelector("#add-button");
@@ -200,54 +247,4 @@ input.addEventListener("keyup", function (event) {
     event.preventDefault();
     document.getElementById("add-button").click();
   }
-});
-
-// this will check if the localStorage contain information and if it is it will print it
-window.addEventListener("DOMContentLoaded", async function () {
-  getData().then((result) => {
-    let dataFromJSONBin = result;
-    if (dataFromJSONBin["counter"] === undefined) return;
-    let itemsFromJSONBin = dataFromJSONBin["my-todo"];
-    counter = dataFromJSONBin["counter"];
-    itemsArray = itemsFromJSONBin;
-    const counterSpan = document.getElementById("counter");
-    counterSpan.innerText = dataFromJSONBin["counter"];
-    for (let i = 0; i < itemsFromJSONBin.length; i++) {
-      //add the container div
-      const todoContainer = document.createElement("div");
-      todoContainer.classList.add("todo-container");
-      listSection.appendChild(todoContainer);
-
-      // adding data-percentage and class to sort them later by priority
-      todoContainer.setAttribute(
-        "data-percentage",
-        itemsFromJSONBin[i]["todo-priority"]
-      );
-
-      // adding a check box
-      const taskCheck = document.createElement("input");
-      taskCheck.setAttribute("type", "checkbox");
-      taskCheck.className = "taskCheck";
-      todoContainer.appendChild(taskCheck);
-
-      // adding the text
-      const todoText = document.createElement("div");
-      todoText.classList.add("todo-text");
-      todoContainer.appendChild(todoText);
-
-      // adding the time
-      const todoCreatedAt = document.createElement("div");
-      todoCreatedAt.classList.add("todo-created-at");
-      todoContainer.appendChild(todoCreatedAt);
-
-      // adding the priority
-      const todoPriority = document.createElement("div");
-      todoPriority.classList.add("todo-priority");
-      todoContainer.appendChild(todoPriority);
-
-      todoText.innerText = itemsFromJSONBin[i]["todo-text"];
-      todoCreatedAt.innerText = itemsFromJSONBin[i]["todo-created-at"];
-      todoPriority.innerText = itemsFromJSONBin[i]["todo-priority"];
-    }
-  });
 });
